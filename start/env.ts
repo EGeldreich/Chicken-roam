4354683afa720a9e66c7b4c91749930a9823d378
Emmanuel Geldreich <egeldreich@gmail.com>
2025-05-02 14:27:45 +0200

    Add limiter related package

--- start/env.ts
+++ start/env.ts
@@ -49,5 +49,8 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring the limiter package
   |----------------------------------------------------------
   */
-  LIMITER_STORE: Env.schema.enum(['redis', 'memory'] as const)
+  LIMITER_STORE: Env.schema.enum(['redis', 'memory'] as const),
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
 })
