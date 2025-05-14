178ccf36e5ce55580cc3b4215f3b4968fc7dacde
Emmanuel Geldreich <egeldreich@gmail.com>
2025-05-14 13:58:27 +0200

    Refactor limiter configuration and clean up CSS styles for better readability and maintainability

--- config/limiter.ts
+++ config/limiter.ts
@@ -2,21 +2,9 @@ import env from '#start/env'
 import { defineConfig, stores } from '@adonisjs/limiter'
 
 const limiterConfig = defineConfig({
-  default: env.get('LIMITER_STORE'),
+  default: env.get('LIMITER_STORE', 'memory'),
   stores: {
-    /**
-     * Redis store to save rate limiting data inside a
-     * redis database.
-     *
-     * It is recommended to use a separate database for
-     * the limiter connection.
-     */
     redis: stores.redis({}),
-
-    /**
-     * Memory store could be used during
-     * testing
-     */
     memory: stores.memory({}),
   },
 })
